302b7b8e228638e909649fbd4cd830af9a2e02eb
Luiz Eduardo Campos da Silva <luizcampos331@gmail.com>
2022-12-13 21:08:26 -0300

    Fix - refactor notification content unity test

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,15 +1,17 @@
 import { Content } from './content';
 
-test('It should be able to create a notification content', () => {
-  const content = new Content('Testing a new notification content');
+describe('Notification content', () => {
+  it('Should be able to create a notification content', () => {
+    const content = new Content('Testing a new notification content');
 
-  expect(content).toBeTruthy();
-});
+    expect(content).toBeTruthy();
+  });
 
-test('It should not be able to create a notification content with less than 5 characters', () => {
-  expect(() => new Content('Test')).toThrow();
-});
+  it('Should not be able to create a notification content with less than 5 characters', () => {
+    expect(() => new Content('Test')).toThrow();
+  });
 
-test('It should not be able to create a notification content with more than 240 characters', () => {
-  expect(() => new Content('a'.repeat(241))).toThrow();
+  it('Should not be able to create a notification content with more than 240 characters', () => {
+    expect(() => new Content('a'.repeat(241))).toThrow();
+  });
 });
