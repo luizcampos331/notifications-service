9d587cdf14e9737a2412a53b3c9b95866f945307
Luiz Eduardo Campos da Silva <luizcampos331@gmail.com>
2022-12-17 12:18:23 -0300

    Feature - prisma repositories

--- src/infra/database/prisma/repositories/prisma-notification-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notification-repository.ts
@@ -15,7 +15,30 @@ export class PrismaNotificationsRepository implements NotificationsRepository {
       },
     });
 
-    return notification; // TODO resolver retorno
+    if (!notification) {
+      return null;
+    }
+
+    return PrismaNotificationMapper.toDomain(notification);
+  }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    const notifications = await this.prismaService.notification.findMany({
+      where: {
+        recipientId,
+      },
+    });
+
+    return notifications.map(PrismaNotificationMapper.toDomain);
+  }
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    const count = await this.prismaService.notification.count({
+      where: {
+        recipientId,
+      },
+    });
+
+    return count;
   }
 
   async create(notification: Notification): Promise<void> {
